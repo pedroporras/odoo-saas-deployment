f73f2d6311d09eb689bab2c44e2ea8ed6046d1ec
Pedro Porras <fmpedroporras@gmail.com>
2018-11-08 15:20:27 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,14 @@ echo """Implemententacion de Odoo Saas, librerias, modulos y dependencias
 export USER=$1
 export SAAS_VERSION=$2
 
+cd path/to/odoo
+sed -i 's/matches="[^"]*"//g' addons/web/static/src/xml/base.xml
+
 su - ${USER} -c "git clone -b ${SAAS_VERSION}  --single-branch --depth=10 https://github.com/it-projects-llc/odoo-saas-tools.git odoo-repo/odoo-saas-tools"
 su - ${USER} -c "git clone -b ${SAAS_VERSION}  --single-branch --depth=10 https://github.com/OCA/e-commerce.git odoo-repo/e-commerce"
 su - ${USER} -c "git clone -b ${SAAS_VERSION}  --single-branch --depth=10 https://github.com/it-projects-llc/access-addons.git odoo-repo/access-addons"
-pip install -r saas-requirements.txt
+pip install -r odoo-saas-requirements.txt
+
+apt-get install nginx
 
 
