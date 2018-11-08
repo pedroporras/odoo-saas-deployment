d3923d7ae755422958c3725e3a2a94a9c2a2c467
Pedro Porras <fmpedroporras@gmail.com>
2018-11-08 16:53:50 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,25 @@ echo """Implemententacion de Odoo Saas, librerias, modulos y dependencias
 export USER=$1
 export SAAS_VERSION=$2
 
-cd /home/o${USER}/odoo-repo
+cd /home/${USER}/odoo-repo
 sed -i 's/matches="[^"]*"//g' addons/web/static/src/xml/base.xml
-
-su - ${USER} -c "git clone -b ${SAAS_VERSION}  --single-branch --depth=10 https://github.com/it-projects-llc/odoo-saas-tools.git odoo-repo/odoo-saas-tools"
+apt-get update
+su - ${USER} -c "git clone -b ${SAAS_VERSION}  --single-branch --depth=10 https://github.com/it-projects-llc/odoo-saas-tools.git odoo-saas-tools"
 su - ${USER} -c "git clone -b ${SAAS_VERSION}  --single-branch --depth=10 https://github.com/OCA/e-commerce.git odoo-repo/e-commerce"
 su - ${USER} -c "git clone -b ${SAAS_VERSION}  --single-branch --depth=10 https://github.com/it-projects-llc/access-addons.git odoo-repo/access-addons"
-pip install -r odoo-saas-requirements.txt
+wget https://raw.githubusercontent.com/pedroporras/odoo-saas-deployment/master/odoo-saas-requirements.txt -O /tmp/rp-saas.txt
+pip install requests --upgrade
+pip install -r /tmp/rp-saas.txt
 
-apt-get install nginx
+apt-get -y install nginx
+cd /etc/nginx
+wget https://raw.githubusercontent.com/pedroporras/odoo-saas-deployment/master/odoo-saas -O /etc/nginx/sites-enabled/odoo-saas
+mv /etc/nginx/sites-enabled/odoo-saas default
+nginx -t
+service nginx start
+/etc/init.d/nginx reload
+
+
+
 
 
