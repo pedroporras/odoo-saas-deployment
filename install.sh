bdc8ec8dab4b3da6f988b3a0c13f89bc14254e51
Pedro Porras <fmpedroporras@gmail.com>
2018-11-08 15:23:03 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo """Implemententacion de Odoo Saas, librerias, modulos y dependencias
 export USER=$1
 export SAAS_VERSION=$2
 
-cd path/to/odoo
+cd /home/o${USER}/odoo-repo
 sed -i 's/matches="[^"]*"//g' addons/web/static/src/xml/base.xml
 
 su - ${USER} -c "git clone -b ${SAAS_VERSION}  --single-branch --depth=10 https://github.com/it-projects-llc/odoo-saas-tools.git odoo-repo/odoo-saas-tools"
