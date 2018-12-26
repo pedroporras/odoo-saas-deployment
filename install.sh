cf21076d9b3e61ee4be9c16681784a2f82268156
Pedro Porras <fmpedroporras@gmail.com>
2018-12-26 09:55:41 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ export SAAS_VERSION=$2
 cd /home/${USER}/odoo-repo
 sed -i 's/matches="[^"]*"//g' addons/web/static/src/xml/base.xml
 apt-get update
-su - ${USER} -c "git clone -b ${SAAS_VERSION}  --single-branch --depth=10 https://github.com/it-projects-llc/odoo-saas-tools.git odoo-repo/odoo-saas-tools"
+su - ${USER} -c "git clone -b ${SAAS_VERSION}  --single-branch --depth=10 https://github.com/pedroporras/odoo-saas-tools.git odoo-repo/odoo-saas-tools"
 su - ${USER} -c "git clone -b ${SAAS_VERSION}  --single-branch --depth=10 https://github.com/OCA/e-commerce.git odoo-repo/e-commerce"
 su - ${USER} -c "git clone -b ${SAAS_VERSION}  --single-branch --depth=10 https://github.com/it-projects-llc/access-addons.git odoo-repo/access-addons"
 su - ${USER} -c "git clone -b ${SAAS_VERSION}  --single-branch --depth=10 https://github.com/OCA/contract.git odoo-repo/contract"
